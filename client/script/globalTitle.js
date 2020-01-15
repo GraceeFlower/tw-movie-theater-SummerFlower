50889e74c7ce258445bddfe670a05d5846e0f39a
Mc <350464517@qq.com>
2020-01-15 11:24:39 +0800

    [zmc] feat-#1: add download data function and get douban top 250 data

--- client/script/globalTitle.js
+++ client/script/globalTitle.js
@@ -1,4 +1,22 @@
 let header = document.getElementsByTagName("header")[0];
+let BASIC_URL = 'http://127.0.0.1:8888';
+let movieId = '26942674';
+
+const top250 = 'top250';
+let data;
+
+function loadItems(){
+  ajax({
+    url: BASIC_URL + '/v2/movie/' + top250,
+    method:'GET',
+    success:function (responseText){
+      data = responseText;
+      console.log(data);
+    }
+  })
+}
+loadItems();
+
 header.addEventListener("click", function(event){
   let target = event.target;
   switch (target.className){
@@ -9,5 +27,5 @@ header.addEventListener("click", function(event){
     window.location.href="./index.html"
     break;
   }
-    
 })
+
