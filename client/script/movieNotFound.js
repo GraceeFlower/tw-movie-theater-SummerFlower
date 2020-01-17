057814406d1d20ead9c845c4751cf43ef5052235
GraceeFlower <1491644103@qq.com>
2020-01-17 17:48:39 +0800

    [wm] fix-#1: delete the useless code

--- client/script/movieNotFound.js
+++ client/script/movieNotFound.js
@@ -3,9 +3,9 @@ let getIdFromURL = thisURL.split("?")[1];
 let nowSearchContent = getIdFromURL.split("=")[1];
 nowSearchContent = decodeURI(decodeURI(nowSearchContent));
 
-let searchWhat = document.getElementsByClassName("search-what")[0];
-let whatNotFound = document.getElementsByClassName("what-not-found")[0];
+const searchWhat = document.getElementsByClassName("search-what")[0];
+const whatNotFound = document.getElementsByClassName("what-not-found")[0];
 
 topSearchInput.value = nowSearchContent;
-searchWhat.innerHTML = "搜索 " + nowSearchContent;
-whatNotFound.innerHTML = '没有找到关于"' + nowSearchContent + '"的电影，请换个搜索词试试吧';
+searchWhat.innerHTML = `搜索 ${nowSearchContent}`;
+whatNotFound.innerHTML = `没有找到关于 ${nowSearchContent} 的电影，请换个搜索词试试吧`;
